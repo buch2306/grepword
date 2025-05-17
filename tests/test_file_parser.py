3cbb14d1043015734217676e5a35a4d98bbe64fd
Anirudh Buchalli <anirudhbuchalli@Anirudhs-MacBook-Pro.local>
2025-05-16 18:32:11 -0700

    (chore): add .gitignore

--- tests/test_file_parser.py
+++ tests/test_file_parser.py
@@ -1,8 +1,10 @@
 from grepword.file_parser.file_parser import FileParser
 
+
 def test_file_parser():
     file_parser = FileParser("tests/test_data/*.txt")
     assert len(file_parser.files_to_content_mapping) > 0
+
 
 def test_file_parser_read_file():
     file_parser = FileParser("tests/test_data/sample1.txt")
